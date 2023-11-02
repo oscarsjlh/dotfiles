01145031bdaea1149305545e8f2652e8ad698b7d
oscar <oscarsjlh@gmail.com>
2023-11-02 09:41:18 +0000

    add files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,16 +6,33 @@ local TS = {
 }
 
 function TS.config()
-	local telescope = require("telescope")
-	telescope.setup({
-		 extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
+  local telescope = require("telescope")
+  telescope.setup({
+    defaults = {
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+        live_grep = {
+          additional_args = function()
+            return { '--hidden', '--glob', '!**/.git/*' }
+          end
+        },
+        grep_string = {
+          additional_args = function()
+            return { '--hidden', '--glob', '!**/.git/*' }
+          end
+        },
+      },
+    },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                        -- the default case_mode is "smart_case"
-    },
+   },
   }
 })
 		local builtin = require('telescope.builtin')
