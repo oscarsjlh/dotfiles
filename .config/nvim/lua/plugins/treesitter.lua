18c1f4a75449a3830be3244dd80a81d496e4676f
Oscar Sebastia <107030286+oscarsjlh@users.noreply.github.com>
2024-05-26 12:00:22 +0100

    fix telescope and comment. (#3)
    * add new lsp
    
    * add tele ui
    
    * more refactor

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,41 +4,41 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
-	require('nvim-treesitter.configs').setup({
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		autotag = {
-			enable = true
-		},
-		textobjects = {
-			select = {
+		require("nvim-treesitter.configs").setup({
+			highlight = {
 				enable = true,
-				lookahead = true,
-				keymaps = {
-					['af'] = '@function.outer',
-					['if'] = '@function.inner',
-					['ac'] = '@class.outer',
-					['ic'] = '@class.inner',
-				}
+				additional_vim_regex_highlighting = false,
 			},
-  },
-		ensure_installed = {
-			"bash",
-			"go",
-			"python",
-			"dockerfile",
-			"hcl",
-			"html",
-			"jq",
-			"json",
-			"yaml",
-			"vim",
-			"vimdoc",
-			"lua",
-		},
-		auto_install = true,
-	})
-end
+			autotag = {
+				enable = true,
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+			ensure_installed = {
+				"bash",
+				"go",
+				"python",
+				"dockerfile",
+				"hcl",
+				"html",
+				"jq",
+				"json",
+				"yaml",
+				"vim",
+				"vimdoc",
+				"lua",
+			},
+			auto_install = true,
+		})
+	end,
 }
--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,16 +1,15 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = {"*.tf", "*.tfvars"},
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+	callback = function(ev)
+		vim.bo[ev.buf].commentstring = "# %s"
+	end,
+	pattern = { "terraform", "hcl", "terraform-vars" },
 })
-
-
