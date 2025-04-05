1d4307d3cac0e73abcc892b04f9faf032abbef4d
oscar <oscarsjlh@gmail.com>
2025-04-05 13:26:52 +0100

    lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,40 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = {
-        enable = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-      },
-      ensure_installed = {
-        "bash",
-        "go",
-        "python",
-        "dockerfile",
-        "hcl",
-        "html",
-        "jq",
-        "json",
-        "yaml",
-        "vim",
-        "vimdoc",
-        "lua",
-      },
-      auto_install = true,
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"bash",
+				"go",
+				"python",
+				"dockerfile",
+				"hcl",
+				"html",
+				"jq",
+				"json",
+				"yaml",
+				"vim",
+				"vimdoc",
+				"lua",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>",
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
+		})
+	end,
 }
