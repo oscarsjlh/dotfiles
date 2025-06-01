f98b0bd43fdb3d551c46f35d77afd38d12281f22
oscar <oscarsjlh@gmail.com>
2025-06-01 08:35:11 +0100

    add bits

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,28 @@
 return {
-  "catppuccin/nvim",
-  "navarasu/onedark.nvim",
-  "Mofiqul/dracula.nvim",
-  "rebelot/kanagawa.nvim",
-  "EdenEast/nightfox.nvim",
-  "shaunsingh/nord.nvim",
-  "sainnhe/gruvbox-material",
-  "sainnhe/everforest",
-  "navarasu/onedark.nvim",
-  "folke/tokyonight.nvim",
-  "shaunsingh/solarized.nvim",
-  "rose-pine/neovim",
-  name = "rose-pine",
-  "nyoom-engineering/oxocarbon.nvim",
+	"catppuccin/nvim",
+	"navarasu/onedark.nvim",
+	"Mofiqul/dracula.nvim",
+	"rebelot/kanagawa.nvim",
+	"EdenEast/nightfox.nvim",
+	"shaunsingh/nord.nvim",
+	"sainnhe/gruvbox-material",
+	enabled = true,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_transparent_background = 1
+		vim.g.gruvbox_material_foreground = "mix"
+		vim.g.gruvbox_material_background = "hard"
+		vim.g.gruvbox_material_ui_contrast = "high"
+		vim.g.gruvbox_material_float_style = "bright"
+		vim.g.gruvbox_material_statusline_style = "material"
+		vim.g.gruvbox_material_cursor = "auto"
+		vim.cmd.colorscheme("gruvbox-material")
+	end,
+	"sainnhe/everforest",
+	"navarasu/onedark.nvim",
+	"folke/tokyonight.nvim",
+	"shaunsingh/solarized.nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
+	"nyoom-engineering/oxocarbon.nvim",
 }
