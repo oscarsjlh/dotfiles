1d9dd7bfbbb4dc52b9e3b0210a058652e817396f
oscar <oscarsjlh@gmail.com>
2026-03-19 19:04:12 +0000

    Use neogit

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+	"NeogitOrg/neogit",
+	lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+
+		-- Only one of these is needed.
+		"sindrets/diffview.nvim", -- optional
+	},
+	cmd = "Neogit",
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	},
+}
